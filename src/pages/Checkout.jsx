e914af566bae99ce839806929d2e7eb304d57574
Adnan <adbak.dev@gmail.com>
2024-03-05 23:39:39 +0100

    log warn

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,5 +1,33 @@
-const Checkout = () => (
-  <h1 className="text-3xl font-bold underline">Checkout</h1>
-);
+import { useSelector } from "react-redux";
+import { CheckoutForm, SectionTitle, CartTotals } from "../components";
+import { toast } from "react-toastify";
+import { redirect } from "react-router-dom";
+
+export const loader = (store) => () => {
+  const user = store.getState().userState.user;
+
+  if (!user) {
+    toast.warn("You need to be logged in to checkout.");
+    return redirect("/login");
+  }
+  return null;
+};
+
+const Checkout = () => {
+  const cartTotal = useSelector((state) => state.cart.items);
+  if (cartTotal === 0) {
+    return <SectionTitle text="Your cart is empty" />;
+  }
+
+  return (
+    <>
+      <SectionTitle text="place your order" />
+      <div className="mt-8 grid gap-8 md:grid-cols-2 items-start">
+        <CheckoutForm />
+        <CartTotals />
+      </div>
+    </>
+  );
+};
 
 export default Checkout;
